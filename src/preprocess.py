207054fec879efc8bc0b9c6eb9b41e0c068ac851
Dheemanth <dheemanth@Dheemanths-MacBook-Air.local>
2024-08-05 04:15:40 +0530

    added the mlflow to the ml experiments

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class Preprocessor:
-    def __init__(self, filepath=None, dataframe=None, delimiter=';'):
+    def __init__(self, filepath=None, dataframe=None, delimiter=";"):
         self.filepath = filepath
         self.delimiter = delimiter
         self.df = dataframe
@@ -15,11 +15,11 @@ class Preprocessor:
     def preprocess_data(self):
         # Find high correlation features
         corr_matrix = self.df.corr().abs()
-        upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1)
-                                  .astype(np.bool_))
-        self.to_drop = [column for column in upper.columns if any(upper[column]
-                                                                  > 0.8)]
-
+        upper = corr_matrix.where(
+            np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool_)
+        )
+        self.to_drop = [column for column in upper.columns if any(
+            upper[column] > 0.8)]
         # Drop high correlation features
         self.df = self.df.drop(columns=self.to_drop)
 
@@ -27,11 +27,13 @@ class Preprocessor:
         X = self.df.drop([target_column], axis=1)
         y = self.df[target_column]
         from sklearn.model_selection import train_test_split
-        return train_test_split(X, y, test_size=test_size,
-                                random_state=random_state, stratify=y)
+
+        return train_test_split(
+            X, y, test_size=test_size, random_state=random_state, stratify=y
+        )
 
     def get_to_drop(self):
         return self.to_drop
-    
+
     def get_dataFrame(self):
         return self.df
